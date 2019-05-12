3a525cf4c90ed7e65b89b801ca65613a814b3835
sandyvmarques <aluno19997@ipt.pt>
2019-05-12 17:38:22 +0100

    Alteração do import para import android.hardware.Camera;

--- app/src/main/java/com/a/customcamera/MainActivity.java
+++ app/src/main/java/com/a/customcamera/MainActivity.java
@@ -1,6 +1,6 @@
 package com.a.customcamera;
 
-import android.graphics.Camera;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.FrameLayout;
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         fl = (FrameLayout) findViewById(R.id.framLayout);
 
         //abrir a camera
-        //camera = Camera.open();
+        camera = Camera.open();
         showCamera = new  ShowCamera(this,camera);
     }
 }
--- app/src/main/java/com/a/customcamera/ShowCamera.java
+++ app/src/main/java/com/a/customcamera/ShowCamera.java
@@ -1,20 +1,26 @@
 package com.a.customcamera;
 
 import android.content.Context;
-import android.graphics.Camera;
+import android.hardware.Camera;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
 
    Camera c;
+   SurfaceHolder holder;
+
     public ShowCamera(Context context, Camera c) {
         super(context);
         this.c=c;
+        holder= getHolder();
+        holder.addCallback(this);
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
+        //param da camera
+
 
     }
 
